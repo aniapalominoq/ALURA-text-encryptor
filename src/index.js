7c475ca6ab01b4b28ae311e7b8f1adc5c0de1f65
Melania Palomino <aniapalominoq@gmail.com>
2024-08-27 15:56:46 -0500

    ADD leemos el DOM para darle la funcionalidad de los botones encrypt y decrypt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 //here we will read the elements of the DOM
-import encryption from "./encryption";
-console.log(encryption.encode("melania palomino"));
+import encryption from "./encryption.js";
+//variables
+
+const encryptButton=document.getElementById("encrypt-button");
+const decryptButton=document.getElementById("decrypt-button");
+//events
+document.getElementById("input-textarea").addEventListener("keyup", function() {
+    this.value = this.value.toLowerCase();
+});
+encryptButton.addEventListener("click", () =>{
+    const text=document.getElementById("input-textarea").value
+    document.getElementById("output-textarea").value=encryption.encode(text)
+    document.getElementById("input-textarea").value=" "
+
+})
+decryptButton.addEventListener("click", () =>{
+    const text=document.getElementById("input-textarea").value
+    document.getElementById("output-textarea").value=encryption.decode(text)
+    
+
+})
+
